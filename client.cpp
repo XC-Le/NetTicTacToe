44707a722a6895c020c135078ff3477579061d2b
XC-Le <superxavier100@gmail.com>
2026-04-01 13:48:30 -0500

    implemented WinSock

--- client.cpp
+++ client.cpp
@@ -14,6 +14,8 @@
 
 #pragma comment(lib, "ws2_32.lib")
 
+using namespace std;
+
 #define PORT 12345
 #define BUFFER_SIZE 1024
 
@@ -21,15 +23,47 @@ char board[9];
 char mySymbol = '?';
 
 void printBoard() {
-    std::cout << "\n";
+    cout << "\n";
     for (int i = 0; i < 9; i++) {
         char c = board[i];
-        if (c == '1') std::cout << " X ";
-        else if (c == '2') std::cout << " O ";
-        else std::cout << " " << i << " "; 
+        if (c == '1') cout << " X ";
+        else if (c == '2') cout << " O ";
+        else cout << " " << i << " "; 
 
-        if (i % 3 != 2) std::cout << "|";
-        else if (i < 8) std::cout << "\n---+---+---\n";
+        if (i % 3 != 2) cout << "|";
+        else if (i < 8) cout << "\n---+---+---\n";
     }
-    std::cout << "\n\n";
+    cout << "\n\n";
+}
+
+int main(){
+
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        cerr << "WSAStartup failed.\n";
+        return 1;
+    }
+    
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == INVALID_SOCKET) {
+        std::cerr << "Socket creation failed.\n";
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+
+    if (connect(sock, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        std::cerr << "Connection failed.\n";
+        WSACleanup();
+        return 1;
+    }
+ 
+    std::cout << "Connected to server!\n";
+
+    
+    return 0;   
 }
--- server.cpp
+++ server.cpp
@@ -14,12 +14,82 @@
 
 #pragma comment(lib, "ws2_32.lib")
 
+using namespace std;
+
 #define PORT 12345
 #define BUFFER_SIZE 1024
 
-// ---- Game State ----
+// game board
 char board[9];
 
+// initialized the board
 void initBoard() {
     for (int i = 0; i < 9; i++) board[i] = ' ';
 }
+
+// returns board in a string
+string boardToString(){
+    return string(board, 9);
+}
+
+// checks if move is valid
+bool isValidMove(int pos){
+    return pos>=0 && pos<9 && board[pos] == ' ';
+}
+
+// applies move to board
+void applyMove(int pos, char symbol){
+    board[pos] = symbol;
+}
+
+// sends a message to client based off sock
+void sendMsg(SOCKET sock, string& msg){
+    send(sock, msg.c_str(), (int) msg.size(), 0);
+}
+
+// receives a message from sock
+string receiveMsg(SOCKET sock){
+    char buf[BUFFER_SIZE] = {0};
+    int bytes = recv(sock, buf, BUFFER_SIZE-1, 0);
+    if(bytes <= 0) return "";
+    return string(buf, bytes);
+}
+
+
+int main(){
+    // initializes WinSock
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        cerr << "WSAStartup failed.\n";
+        return 1;
+    }
+
+    // creates a socket to listen on
+    SOCKET ListenSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (ListenSocket == INVALID_SOCKET) {
+            printf("Error at socket(): %ld\n", WSAGetLastError());
+            WSACleanup();
+            return 1;
+    }
+
+    // creates address and binds to the socket and port
+    sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(PORT);
+
+    bind(ListenSocket, (sockaddr*)&addr, sizeof(addr));
+    listen(ListenSocket, 2);
+
+
+    SOCKET ClientSocket1 = accept(ListenSocket, nullptr, nullptr);
+
+    SOCKET ClientSocket2 = accept(ListenSocket, nullptr, nullptr);
+
+
+    return 0;
+}
+
+// determine player 1
+
+// determine player 2
